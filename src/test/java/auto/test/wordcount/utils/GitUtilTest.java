4f52335854c365fb26b34d53b607ba42ac22fc03
GreyZeng <410486047@qq.com>
2021-03-02 23:35:36 +0800

    fix unit test

--- src/test/java/auto/test/wordcount/utils/GitUtilTest.java
+++ src/test/java/auto/test/wordcount/utils/GitUtilTest.java
@@ -3,9 +3,7 @@ package auto.test.wordcount.utils;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import static auto.test.wordcount.utils.FileUtil.generateEmptyFile;
 
@@ -18,9 +16,9 @@ public class GitUtilTest {
         String[] fileB = generateEmptyFile();
         GitUtil.cloneRepo(url, fileA[0], true);
         GitUtil.cloneRepo(url, fileB[0], false);
-        System.out.println("完成下载，接下来会休眠60秒，请手动查看Git仓库是否正常下载下来，休眠60秒以后，clone的文件会删除！！！！！");
-        TimeUnit.SECONDS.sleep(60);
+        System.out.println("完成下载，请手动查看Git仓库是否正常下载下来，如果正常下载下来了，请删除下载的文件，不要提交到Git上");
+        /*TimeUnit.SECONDS.sleep(60);
         FileUtil.deleteFile(new File(fileA[0]));
-        FileUtil.deleteFile(new File(fileB[0]));
+        FileUtil.deleteFile(new File(fileB[0]));*/
     }
 }
